91cc5d46c5adb69afa6e3bb4cf02e3c11c2642cd
bazz <mbazzinotti@gmail.com>
2013-12-30 00:59:57 -0500

    Added erasethread files

---
+++ SuperFlashUsb/erasethread.cpp
@@ -0,0 +1,94 @@
+#include "erasethread.h"
+#include "pc/sflash.h"
+
+void EraseThread::specialStart(int blocknum_start, int blocknum_end)
+{
+
+
+
+
+}
+
+void EraseThread::run ()
+{
+  end = false;
+
+  this->Erase();
+}
+
+void EraseThread::Erase()
+{
+    //emit setProgress()
+    this->EraseBlocks(blocknum_start,blocknum_end);
+}
+
+void EraseThread::EraseBlocks(int blocknum_start, int blocknum_end)
+{
+    for (; blocknum_start <= blocknum_end; blocknum_start++)
+    {
+        this->EraseBlock(blocknum_start);
+    }
+}
+
+
+void EraseThread::EraseBlock(int blocknum)
+{
+    redo:
+    r = libusb_control_transfer(dev_handle, LIBUSB_RECIPIENT_DEVICE|LIBUSB_REQUEST_TYPE_VENDOR|LIBUSB_ENDPOINT_OUT,ERASE, ERASE_BLOCK_USING_BLOCKNUM, (uint16_t) blocknum, NULL, 0, 500);
+    if(r == 0 )
+    {
+        cout << "Erase Cmd Sent, Verifying: \n";
+
+        while (VerifyErase() != 0);
+    }
+    else if (r == LIBUSB_ERROR_PIPE || r == LIBUSB_ERROR_OTHER)
+    {
+        cout << "EraseBlockUsingAddress: PIPE/Other Error: " << r << endl;
+        printf( "%d\n", errno );
+        //ResetAddress();
+        goto redo;
+    }
+    else
+    {
+        cout<<"EraseBlockUsingAddress Error: " << r <<endl;
+        printf( "%d\n", errno );
+        goto redo;
+    }
+}
+
+
+
+int EraseThread::VerifyErase()
+{
+    redo:
+    r = libusb_control_transfer(dev_handle, LIBUSB_RECIPIENT_DEVICE|LIBUSB_REQUEST_TYPE_VENDOR|LIBUSB_ENDPOINT_IN,ERASE, ERASE_BLOCK_USING_ADDRESS, 0x0000, &data[0], 1, 500);
+    if(r == 1 )
+    {
+        //cout << "Erase Status Byte: ";
+        //printf ("%x\n", data[0]);
+
+        if (data[0] == 0x80)
+        {
+            cout << "Complete!\n";
+            SetLEDWithByte(0);
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else if (r == LIBUSB_ERROR_PIPE || r == LIBUSB_ERROR_OTHER)
+    {
+        cout << "EraseBlockUsingAddress: PIPE/Other Error: " << r << endl;
+        printf( "%d\n", errno );
+        //ResetAddress();
+        goto redo;
+    }
+    else
+    {
+        cout<<"EraseBlockUsingAddress Error: " << r <<endl;
+        printf( "%d\n", errno );
+        goto redo;
+    }
+}
---
+++ SuperFlashUsb/erasethread.h
@@ -0,0 +1,50 @@
+#ifndef ERASETHREAD_H
+#define ERASETHREAD_H
+
+#include <QFile>
+#include <QThread>
+#include "pc/main.h"
+#include "utility.h"
+
+class EraseThread : public QThread
+{
+    Q_OBJECT
+public:
+    EraseThread()
+    {
+    }
+    ~EraseThread()
+    {
+
+    }
+
+    virtual void run();
+    void specialStart(int blocknum_start, int blocknum_end);
+
+    void Erase();
+    int VerifyErase();
+    void EraseBlocks(int blocknum_start, int blocknum_end);
+    //void EraseBlockUsingAddress(unsigned long block_address);
+    void EraseBlock(int blocknum);
+
+
+
+
+
+
+    bool end;
+    //QFile *file;
+
+public slots:
+    void canceled (void)
+    {
+      end = true;
+    }
+
+signals:
+    void setProgress(int x);
+    void message(int msgtype, QString title, QString msg);
+};
+
+
+#endif // ERASETHREAD_H
