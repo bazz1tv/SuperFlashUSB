19f0e3afe9a39f2dee619a52d52e9346712a1959
bazz <mbazzinotti@gmail.com>
2013-12-31 03:02:46 -0500

    DERP

--- SuperFlashUsb/programcartthread.cpp
+++ SuperFlashUsb/programcartthread.cpp
@@ -8,4 +8,10 @@ void ProgramCartThread::run()
 void ProgramCartThread::specialStart(ROM_t *rom1, ROM_t *rom2, ROM_t *rom3, ROM_t *rom4)
 {
 
+// do this even if Game 4
+    // after it's all said and done
+    // fill the next game table entry
+     // fseek (destfile, 0x4000 + (file_no - 1) * 0x20, SEEK_SET);
+     // fputc (0, destfile);                          // indicate no next game
+      //fclose (destfile);
 }
