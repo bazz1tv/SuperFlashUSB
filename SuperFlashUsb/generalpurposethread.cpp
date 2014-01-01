24f92517f9102d682789b32508270b2f4514fdcb
bazz <mbazzinotti@gmail.com>
2013-12-31 22:10:13 -0500

    DERP

--- SuperFlashUsb/generalpurposethread.cpp
+++ SuperFlashUsb/generalpurposethread.cpp
@@ -1,5 +1,10 @@
 #include "generalpurposethread.h"
 
+void GeneralPurposeThread::specialStart(int operation)
+{
+    if (operation == )
+}
+
 void GeneralPurposeThread::run ()
 {
   end = false;
@@ -9,8 +14,17 @@ void GeneralPurposeThread::run ()
   emit setEnabledButtons(true);
 }
 
-void
-GeneralPurposeThread::canceled (void)
+void GeneralPurposeThread::canceled (void)
 {
   end = true;
 }
+
+// LET'S ESTABLISH AN ORDER OF OPERATIONS FOR WHEN THE PROGRAMMER IS PLUGGED IN AT STARTUP or WHATNOT
+
+/* First, let's get the Chip ID Babayyyy. if it doesn't match. ASK the user if he is not using a 64MB model
+
+
+  Why don't we query the 32 MB chip ID as well. and suggest if that is the case, that 32MB support is not yet implemented
+
+
+  */
